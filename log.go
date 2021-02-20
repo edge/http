f07b34b9623e25442d6c9c009675b2bbf0ac4bd6
Aneurin Barker Snook <abs@maedan.org>
2021-02-20 21:51:25 +0000

    logger fixes

--- log.go
+++ log.go
@@ -7,11 +7,13 @@ import (
 )
 
 type logHandler struct {
-	log  zerolog.Logger
-	next http.Handler
+	level zerolog.Level
+	log   zerolog.Logger
+	next  http.Handler
 }
 
 type logWriter struct {
+	level      zerolog.Level
 	log        zerolog.Logger
 	req        *http.Request
 	statusCode int
@@ -19,16 +21,18 @@ type logWriter struct {
 }
 
 // Log creates a handler middleware that logs each HTTP connection passing through it.
-func Log(log zerolog.Logger, next http.Handler) Handler {
+func Log(log zerolog.Logger, level zerolog.Level, next http.Handler) Handler {
 	h := &logHandler{
-		log:  log,
-		next: next,
+		level: level,
+		log:   log,
+		next:  next,
 	}
 	return Always(h)
 }
 
 func (h *logHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	lw := &logWriter{
+		level:      h.level,
 		log:        h.log,
 		req:        req,
 		statusCode: http.StatusOK,
@@ -42,25 +46,26 @@ func (lw *logWriter) Header() http.Header {
 }
 
 func (lw *logWriter) Write(b []byte) (int, error) {
-	if lw.statusCode < http.StatusInternalServerError {
-		lw.log.Info().
-			Str("method", lw.req.Method).
-			Str("path", lw.req.URL.Path).
-			Int("size", len(b)).
-			Int("status", lw.statusCode).
-			Msg("ok")
+	var evt *zerolog.Event
+	if lw.isError() {
+		evt = lw.log.Error()
 	} else {
-		lw.log.Error().
-			Str("method", lw.req.Method).
-			Str("path", lw.req.URL.Path).
-			Int("size", len(b)).
-			Int("status", lw.statusCode).
-			Msg(string(b))
+		evt = lw.log.WithLevel(lw.level)
 	}
+	size := len(b)
+	evt.Str("method", lw.req.Method).
+		Str("path", lw.req.URL.Path).
+		Int("status", lw.statusCode).
+		Msgf("(%dB)", size)
+
 	return lw.w.Write(b)
 }
 
 func (lw *logWriter) WriteHeader(status int) {
 	lw.statusCode = status
 	lw.w.WriteHeader(status)
+}
+
+func (lw *logWriter) isError() bool {
+	return lw.statusCode >= http.StatusInternalServerError
 }
